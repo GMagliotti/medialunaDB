90375ed2fe5a28d78a9c7c5033c20c3c01f70e8f
FSendot <fsendot@itba.edu.ar>
2024-11-16 00:23:55 -0300

    Add conditional insert for checking duplicate info

--- populate_dbs.py
+++ populate_dbs.py
@@ -2,7 +2,7 @@ import pandas as pd
 from persistence.cassandra_connection import CassandraConnection
 from models.invoice_by_client import InvoiceByClient
 from models.invoice_by_product import InvoiceByProduct
-from datetime import datetime
+from cassandra.cqlengine.query import LWTException
 
 
 def main():
@@ -18,27 +18,29 @@ def main():
     cassandra_client = CassandraConnection()
     cassandra_client.set_default_keyspace("invoices")
     for index, row in joined_invoice.iterrows():
-        print(row)
-        InvoiceByClient.create(
-            client_id=row["nro_cliente"],
-            product_id=row["codigo_producto"],
-            date=row["fecha"],
-            invoice_id=row["nro_factura"],
-            item_number=row["nro_item"],
-            total_with_tax=row["total_con_iva"],
-            tax=row["iva"],
-            amount=row["cantidad"],
-        )
-        InvoiceByProduct.create(
-            client_id=row["nro_cliente"],
-            product_id=row["codigo_producto"],
-            date=row["fecha"],
-            invoice_id=row["nro_factura"],
-            item_number=row["nro_item"],
-            total_with_tax=row["total_con_iva"],
-            tax=row["iva"],
-            amount=row["cantidad"],
-        )
+        try:
+            InvoiceByClient.if_not_exists().create(
+                client_id=row["nro_cliente"],
+                product_id=row["codigo_producto"],
+                date=row["fecha"],
+                invoice_id=row["nro_factura"],
+                item_number=row["nro_item"],
+                total_with_tax=row["total_con_iva"],
+                tax=row["iva"],
+                amount=row["cantidad"],
+            )
+            InvoiceByProduct.if_not_exists().create(
+                client_id=row["nro_cliente"],
+                product_id=row["codigo_producto"],
+                date=row["fecha"],
+                invoice_id=row["nro_factura"],
+                item_number=row["nro_item"],
+                total_with_tax=row["total_con_iva"],
+                tax=row["iva"],
+                amount=row["cantidad"],
+            )
+        except LWTException as e:
+            print(e.existing)
 
 
 if __name__ == "__main__":
